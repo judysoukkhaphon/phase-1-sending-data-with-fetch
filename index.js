df7066919a0578236ba6ac65df272332f25c7f8c
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-02-28 13:21:36 -0500

    did code along exactly but did not pass all the tests for some reason, tried to write full code version of the function arrows in the video  but am not getting the same test results. Consider going back and figuring it out

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+/*
+function submitData(name, email) {
+
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        }),
+    })
+            .then(function (response) {
+                return response.json();
+            })
+                .then(function (object) {
+                   return object.document.body.innerHTML = `<p>${response.id}</p>`
+
+                })
+                    .catch(function (error) {
+                        console.log("error", error);
+                        return (document.body.innerHTML = `<p>${error.message}</p>`);
+                    })
+                    
+}
+
+*/
+
+function submitData(name, email) {
+
+    return fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then(response => {
+        document.body.innerHTML = `<p>${response.id}</p>`
+    })
+    .catch(error => {
+        console.log("error", error)
+        document.body.innerHTML = `<p>${error.message}</p>`
+    })
+
+}
+
+
+
+
+
+
